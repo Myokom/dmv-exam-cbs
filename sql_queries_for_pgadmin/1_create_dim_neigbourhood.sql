0d1e100cf16956caa895ce95d80976e6ac7dffb2
“Tobias” <tobi_m@live.dk>
2024-12-29 11:58:34 +0100

    minor things :)

--- sql_queries_for_pgadmin/1_create_dim_neigbourhood.sql
+++ sql_queries_for_pgadmin/1_create_dim_neigbourhood.sql
@@ -1,21 +1,21 @@
 --- GUIDE ---
 --- NOTE THIS IS A TABLE --- Not sure if I couldn've made it as a view since I create the ID column 
 
---- 1. CREATE
-CREATE TABLE IF NOT EXISTS public.dimension_neighborhood (
+--- 1. DROP EXISTING TABLE (if exists)
+DROP TABLE IF EXISTS public.dimension_neighborhood CASCADE; --- CASCADE is used to drop all objects that depend on the table, note this includes fact_listings
+
+--- 2. CREATE NEW TABLE
+CREATE TABLE public.dimension_neighborhood (
     neighborhood_id SERIAL PRIMARY KEY, -- Auto-incremented ID
-    neighborhood TEXT,
-    neighborhood_cleansed TEXT
+    neighborhood TEXT
 );
 
-
---- 2. POPULATE WITH DATA
-INSERT INTO public.dimension_neighborhood (neighborhood, neighborhood_cleansed)
+--- 3. POPULATE WITH DATA
+INSERT INTO public.dimension_neighborhood (neighborhood)
 SELECT DISTINCT
-    neighbourhood AS neighborhood,
-    neighbourhood_cleansed AS neighborhood_cleansed
+    neighbourhood_cleansed AS neighborhood
 FROM public.listings
 WHERE neighbourhood_cleansed IS NOT NULL;
 
---- 3. TEST, select all
-select * from dimension_neighborhood
+--- 4. TEST, SELECT ALL
+SELECT * FROM public.dimension_neighborhood;
